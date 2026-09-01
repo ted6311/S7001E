95a848f1e4aa4b479f6dee548ef85837c455d6ac
ted6311 <theodor.lignell@gmail.com>
2026-09-01 17:32:02 +0200

    commit 1, started with lab 1

---
+++ Lab 1/q1.m
@@ -0,0 +1,51 @@
+
+clear; clc; 
+
+%% a)
+
+
+
+
+function [a, b, roll] = roll_dice(N)
+    p = [1/6 1/6 1/6 1/6 1/6 1/6 ]; % probability of each face
+    P = [0 cumsum(p)] ;
+    roll = zeros(N,1); % vector to hold results of each roll
+
+    for i=1:N % loop over number of rolls
+        x=rand(1,1);
+        for j=2:length(P) % determine result of each roll
+            if( (P(j-1)<x) & (x<P(j)) )
+                roll(i) = j-1;
+                break
+            end
+        end
+    end
+    a = zeros(1,6);
+    for i = 1:6
+        a(i) = sum(roll == i);
+    end
+    b = a/N;
+    
+end
+
+[a,b, roll] = roll_dice(1000);
+disp("Fraction/side"), disp(b)
+% Function works
+% And we get the fraction for each occurence
+
+%% b) 
+
+%Hist
+
+figure; grid on; 
+hist(roll, 6)
+xlabel("bins")
+ylabel("Throws")
+
+
+%PDF
+P = [1/6 1/6 1/6 1/6 1/6 1/6 ];
+figure; 
+bar(P);
+xlabel("Side")
+ylabel("Theoretical Probability")
