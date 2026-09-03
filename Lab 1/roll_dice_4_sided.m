1302fbbba84eab4900181905d6bae4e02f0ba4ea
ted6311 <theodor.lignell@gmail.com>
2026-09-03 17:58:55 +0200

    seperated functions to seperate scripts

---
+++ Lab 1/roll_dice_4_sided.m
@@ -0,0 +1,21 @@
+function [a, b, roll, N] = roll_dice_4_sided(N)
+    p = [0.1 0.2 0.4 0.3]; % probability of each face
+    P = [0 cumsum(p)] ;
+    roll = zeros(N,1); % vector to hold results of each roll
+
+    for i=1:N % loop over number of rolls
+        x=rand(1,1);
+        for j=2:length(P) % determine result of each roll
+            if( (P(j-1)<x) && (x<P(j)) )
+                roll(i) = j-1;
+                break
+            end
+        end
+    end
+    a = zeros(1,4);
+    for i = 1:4
+        a(i) = sum(roll == i);
+    end
+    b = a/N;
+
+end
