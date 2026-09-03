80125e87ee890c9ee2a89cf28454e24c9aad5623
ted6311 <theodor.lignell@gmail.com>
2026-09-03 10:59:35 +0200

    a)-b) done, c) needs cumsum and hist instead

--- Lab 1/q2.m
+++ Lab 1/q2.m
@@ -27,3 +27,33 @@ end
 
 [a,b, roll, N] = roll_dice_4_sided(1000);
 
+%% a) repeat 1a) for this 4 sided dice
+
+disp("Fraction/side"), disp(b)
+% Function works
+% And we get the fraction for each occurence
+
+
+%% b) repeat 1b)
+
+%Hist
+
+figure; grid on; 
+hist(roll, 4)
+xlabel("bins")
+ylabel("Throws")
+
+
+%PDF
+P = [0.1 0.2 0.4 0.3]; 
+figure; 
+bar(P);
+xlabel("Side")
+ylabel("Theoretical Probability")
+
+%% c) 
+figure;
+histogram(roll, 4,  'Normalization', 'probability')
+xlabel("Side");
+ylabel("estimated Probability");
+title("Estimated vs Theoretical probability");
