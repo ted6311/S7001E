bdb1853a1e82b20cd2177805e17ac55dccb52f4d
ted6311 <theodor.lignell@gmail.com>
2026-09-26 17:28:01 +0200

    soundfiles added to git and matlab

--- Lab 2/q4.m
+++ Lab 2/q4.m
@@ -68,9 +68,9 @@ title('Scatter plot Y_1 vs. Y_2');
 
 
 %% f)
-int = @(v) (1/sqrt(2*pi)) * exp(-0.5 * v.^2) .* normcdf((4*sqrt(3)/5).*v - sqrt(6)/10);
+% int = @(v) (1/sqrt(2*pi)) * exp(-0.5 * v.^2) .* normcdf((4*sqrt(3)/5).*v - sqrt(6)/10);
 
-int2 = integral(int, -1.1547, -0.5774);
+% int2 = integral(int, -1.1547, -0.5774);
 
 
 %% g)
